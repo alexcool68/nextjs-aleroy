3b564837efa9cdfd01380722f19e3e63232037f5
Alexis LEROY <alexcool68@free.fr>
2024-03-09 12:06:57 +0100

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
+
 // import { Inter } from 'next/font/google';
 import './globals.css';
 import ConvexClientProvider from './ConvexClientProvider';
@@ -31,6 +33,7 @@ export default function RootLayout({
                     <Header />
                     {children}
                     <Footer />
+                    <Analytics />
                 </ConvexClientProvider>
             </body>
         </html>
