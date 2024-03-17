f9e3e175ebb7c608d5526bf6f573f1e5561d53e3
Alexis LEROY <alexcool68@free.fr>
2024-03-17 13:26:30 +0100

    fix

--- src/app/dashboard/test/page.tsx
+++ src/app/dashboard/test/page.tsx
@@ -4,18 +4,12 @@ import { useEffect } from 'react';
 
 import TitleHeader from '../_components/title-header';
 
-import TipTapEditor from '../(dashboard)/articles/_components/tip-tap-editor';
-
 function TestingDashboard() {
     useEffect(() => {}, []);
 
     return (
         <div className="p-5">
             <TitleHeader title="Testing"></TitleHeader>
-
-            <div className="w-full p-3 max-h-[600px] overflow-auto border rounded-xl">
-                <TipTapEditor />
-            </div>
         </div>
     );
 }
