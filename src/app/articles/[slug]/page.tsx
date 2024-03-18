46656ebe9d3c07c3dd466e8b48d4cce5e5162f2a
Alexis LEROY <alexcool68@free.fr>
2024-03-18 16:59:48 +0100

    image cover

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 
 import { Button } from '@/components/ui/button';
+import { getImageUrl } from '@/lib/utils';
 
 export default function Page({ params }: { params: { slug: string } }) {
     const article = useQuery(api.articles.getArticleBySlug, { slug: params.slug });
@@ -17,8 +19,20 @@ export default function Page({ params }: { params: { slug: string } }) {
     return (
         <>
             <div className="container mx-auto p-8">
+                {article?.imgId && (
+                    <div className="relative max-w-full">
+                        <Image
+                            src={getImageUrl(article?.imgId)}
+                            alt="article image"
+                            sizes="100vh"
+                            width={0}
+                            height={0}
+                            className="w-full max-h-64 rounded-xl opacity-65 object-cover"
+                        />
+                    </div>
+                )}
                 {article && (
-                    <div className="flex flex-col items-center justify-start gap-10">
+                    <div className="flex flex-col items-center justify-start gap-10 mt-5">
                         <h1 className="text-7xl items-center text-center">{article.title}</h1>
 
                         <div className="article article-invert article-xl" dangerouslySetInnerHTML={{ __html: article.content }} />
